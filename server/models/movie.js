5b947e893875c02459e8d92b6749da6da73c043c
stephen <lee.stephen@courrier.uqam.ca>
2024-03-06 11:58:25 -0500

    adding movieSchema

---
+++ server/models/movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    Title: String,
+    Year: String,
+    Released: String,
+    Runtime: String,
+    Genre: String,
+    Director: String,
+    Writer: String,
+    Actors: String,
+    Plot: String,
+    Language: String,
+    Country: String,
+    Awards: String,
+    Poster: String,
+    Ratings: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
