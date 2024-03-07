315db7fb88bd5d09f2338b0773edc8759ea94696
stephen <lee.stephen@courrier.uqam.ca>
2024-03-06 20:38:08 -0500

    generateSearchTerm

---
+++ server/routes/addMovie.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Movie = require('../models/Movie');
+const axios = require('axios');
+const { faker } = require('@faker-js/faker');
+
+const getRandomLetter = () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const randomIndex = Math.floor(Math.random() * alphabet.length);
+  return alphabet[randomIndex];
+};
+
+const generateSearchTerm = async () => {
+  const randomWord = faker.word.words(1); //getRandomLetter() + getRandomLetter();
+  return randomWord;
+};
+
+const fetchDataAndStoreInDB = async () => {
+  const MovieToStore = 1;
+  try {
+      for (let i = 0; i < MovieToStore; i++) {
+          const searchTerm = await generateSearchTerm();
+          console.log('searchTerm', searchTerm);
+          const response = await axios.get(`https://www.omdbapi.com/?apikey=53cb3007&t=${searchTerm}`);
+
+          const filteredData = {
+            Title: response.data.Title,
+            Year: response.data.Year,
+            Rated: response.data.Rated,
+            Released: response.data.Released,
+            Runtime: response.data.Runtime,
+            Genre: response.data.Genre,
+            Director: response.data.Director,
+            Writer: response.data.Writer,
+            Actors: response.data.Actors,
+            Plot: response.data.Plot,
+            Language: response.data.Language,
+            Country: response.data.Country,
+            Awards: response.data.Awards,
+            Poster: response.data.Poster,
+          };
+          console.log('filteredData', filteredData);
+
+          await Movie.create(filteredData);
+      }
+
+      console.log('Données stockées avec succès dans la base de données.');
+  } catch (error) {
+      console.error('Erreur lors du stockage des données dans la base de données :', error);
+  } finally {
+      mongoose.connection.close();
+  }
+};
+
+module.exports = {
+  fetchDataAndStoreInDB 
+};
