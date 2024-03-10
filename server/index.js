ea59442a6cb1a0fdde06c1fadec2c11f339cd041
stephen <lee.stephen@courrier.uqam.ca>
2024-03-10 16:04:48 -0400

    .

--- server/index.js
+++ server/index.js
@@ -6,9 +6,8 @@ const UserModel = require('./models/user')
 const app = express();
 const port = process.env.PORT || 5000;
 const auth = require('./routes/user');
-const { addMovieToDatabase, fetchDataAndStoreInDB } = require('./routes/addMovieInDb.js');
-const movieRoutes = require('./routes/movies');
 const { fetchDataAndStoreInDB } = require('./routes/addMovieInDb.js');
+const movieRoutes = require('./routes/movies');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
