56e893dcbe5de34b623df9e3f2f183de742c9a4c
Gabriel Michaud <michaud.gabriel.3@courrier.uqam.ca>
2024-04-21 17:50:49 -0400

    Ajout du bouton pour mettre la watchlist privée

--- client/src/component/Watchlist/Watchlist.js
+++ client/src/component/Watchlist/Watchlist.js
@@ -9,9 +9,10 @@ import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } fr
 const Watchlist = () => {
     const { user } = useAuthContext();
     const [watchlist, setWatchlist] = useState([])
-    const [setError] = useState([])
+    const [error, setError] = useState([])
     const path = window.location.pathname
     const id = path.substring(path.indexOf(':') + 1)
+    const [isPrivate, setIsPrivate] = useState(false);
 
     const removeMovie = async (_id) => {
         console.log(_id)
@@ -21,9 +22,6 @@ const Watchlist = () => {
                 email: user.email,
                 _id: _id
             }
-            // headers: {
-            //     'Auhtorization': `Bearer ${user.token}`
-            // }
         }).then((response) => {
             if (response.data.status === 200) {
                 console.log("Movie removed from wathclist")
@@ -36,20 +34,46 @@ const Watchlist = () => {
         })
     }
 
+    // const updatePrivate = async (isPrivate) => {
+    //     await axios.post('http://localhost:5000/api/user/private', {
+    //         email: user.email,
+    //         private: isPrivate
+    //     })
+    //         .then((res) => {
+    //             if (res.data.status === 200) {
+    //                 console.log("in here with", isPrivate)
+    //                 setIsPrivate(!isPrivate)
+    //                 console.log(!isPrivate)
+    //             } else {
+    //                 setError("Pas Réussi à changer du côté serveur")
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             console.log("Some sort of error", err)
+    //         })
+    // }
+
+    const changeValue = (e) => {
+        e.preventDefault()
+        setIsPrivate(!isPrivate)
+    }
+
     useEffect(() => {
         const fetchWatchlist = async () => {
-            const response = await axios.get(`http://localhost:5000/api/user/watchlist${id}`, {
+            await axios.get(`http://localhost:5000/api/user/watchlist${id}`, {
                 params: {
                     id: id
                 }
-                // headers: {
-                //     'Auhtorization': `Bearer ${user.token}`
-                // }
             })
-            if (response.data.status === 200) {
-                setWatchlist(response.data.movies)
-            }
-            console.log(response.data)
+                .then(watchlist => {
+                    if (watchlist.data.status === 200) {
+                        setWatchlist(watchlist.data.movies)
+                    }
+                    console.log(watchlist.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
         fetchWatchlist()
     }, [id])
@@ -57,48 +81,60 @@ const Watchlist = () => {
     return (
         <div className="App-header">
             <div className='Wrapper'>
-            {user && user.id === id && (
-                <><h1 className='titleAuth'>Votre liste de visionnement</h1><span>
-                    <br />
-                    <FacebookShareButton
-                        url={'http://example.com'}
-                        quote={'Regarder ma liste de visionnement et conseiller moi!'}
-                        hashtag="#movieconsensus"
-                    >
-                        <FacebookIcon size={32} round />
-                        <span className='partage'>Partager liste</span>
-                    </FacebookShareButton>
-                    <span>&emsp;</span>
-                    <TwitterShareButton
-                        url={'http://example.com'}
-                        quote={'Regarder ma liste de visionnement et conseiller moi!'}
-                        hashtag="#movieconsensus"
-                    >
-                        <TwitterIcon size={32} round />
-                        <span className='partage'>Partager liste</span>
-                    </TwitterShareButton>
-                </span></>
-            )}
-            {(!user || (user && user.id !== id)) && (
-                <h1>Liste de visionnement</h1>
-            )}
-            <ul className='watchlist'>
-                {watchlist.map(function (movie, i) {
-                    return <li key={movie._id}>
-                        <Link to={`/movies/${movie._id}`} key={movie._id}>
-                            <img
-                                src={movie.Poster}
-                                alt={`${movie.Title} Poster`}
-                                style={{ width: '160px', height: '240px' }} />
-                            <h2 className='movietitle'>{movie.Title}</h2>
-                        </Link>
-                        <p>{movie.Year} &emsp; {movie.Runtime} &emsp; {movie.Rated} &emsp; {movie.Genre}</p>
-                        <p>Director : {movie.Director} &emsp; Actors : {movie.Actors}</p>
-                        <p>{movie.Plot}</p>
-                        {user && user.id === id && (<Button variant="outline-danger" size='sm' onClick={() => removeMovie(movie._id)}>Supprimer</Button>)}
-                    </li>;
-                })}
-            </ul>
+                {user && user.id === id && (
+                    <><h1 className='titleAuth'>Votre liste de visionnement</h1><span>
+                        <br />
+                        <FacebookShareButton
+                            url={'http://example.com'}
+                            quote={'Regarder ma liste de visionnement et conseiller moi!'}
+                            hashtag="#movieconsensus"
+                        >
+                            <FacebookIcon size={32} round />
+                            <span className='partage'>Partager liste</span>
+                        </FacebookShareButton>
+                        <span>&emsp;</span>
+                        <TwitterShareButton
+                            url={'http://example.com'}
+                            quote={'Regarder ma liste de visionnement et conseiller moi!'}
+                            hashtag="#movieconsensus"
+                        >
+                            <TwitterIcon size={32} round />
+                            <span className='partage'>Partager liste</span>
+                        </TwitterShareButton>
+                    </span>
+                        <span>&emsp;</span>
+                        {isPrivate === false && (
+                            <Button variant="outline-success" onClick={changeValue}>Mettre watchlist privée</Button>
+                        )}
+                        {isPrivate === true && (
+                            <Button variant='success' onClick={changeValue}>Mettre watchlist publique</Button>
+                        )}
+                        {error && (
+                            <p>{error}</p>
+                        )}
+                    </>
+
+                )}
+                {(!user || (user && user.id !== id)) && (
+                    <h1>Liste de visionnement</h1>
+                )}
+                <ul className='watchlist'>
+                    {watchlist.map(function (movie, i) {
+                        return <li key={movie._id}>
+                            <Link to={`/movies/${movie._id}`} key={movie._id}>
+                                <img
+                                    src={movie.Poster}
+                                    alt={`${movie.Title} Poster`}
+                                    style={{ width: '160px', height: '240px' }} />
+                                <h2 className='movietitle'>{movie.Title}</h2>
+                            </Link>
+                            <p>{movie.Year} &emsp; {movie.Runtime} &emsp; {movie.Rated} &emsp; {movie.Genre}</p>
+                            <p>Director : {movie.Director} &emsp; Actors : {movie.Actors}</p>
+                            <p>{movie.Plot}</p>
+                            {user && user.id === id && (<Button variant="outline-danger" size='sm' onClick={() => removeMovie(movie._id)}>Supprimer</Button>)}
+                        </li>;
+                    })}
+                </ul>
             </div>
         </div>
     );
--- server/routes/user.js
+++ server/routes/user.js
@@ -10,103 +10,113 @@ router.use(express.json())
 // router.use(requireAuth)
 
 router.get('/api/user:id', (req, res) => {
-    const id = req.params.id;
-    UserModel.findOne({ _id: id })
-        .then(user => {
-            res.json({ user, status: 200 })
-        })
-        .catch(() => res.json({ msg: "Erreur innatendue côté serveur", status: 500 }))
+  const id = req.params.id;
+  UserModel.findOne({ _id: id })
+    .then(user => {
+      res.json({ user, status: 200 })
+    })
+    .catch(() => res.json({ msg: "Erreur innatendue côté serveur", status: 500 }))
 })
 
 router.delete('/api/user:email', (req, res) => {
-    const email = req.params.email;
-    console.log(email)
-    UserModel.deleteOne({ email: email })
-        .then(() => {
-            res.json({ msg: "Compte utilistateur supprimé", status: 200 })
-        })
-        .catch(() => res.json({ msg: "Erreur innatendue côté serveur", status: 500 }))
+  const email = req.params.email;
+  console.log(email)
+  UserModel.deleteOne({ email: email })
+    .then(() => {
+      res.json({ msg: "Compte utilistateur supprimé", status: 200 })
+    })
+    .catch(() => res.json({ msg: "Erreur innatendue côté serveur", status: 500 }))
+})
+
+router.post('/api/user/private', (req, res) => {
+  const { private, email } = req.body;
+  UserModel.updateOne({ email: email }, {
+    private: private
+  }).then(() => {
+    res.json({ msg: "Changed state private", status: 200 })
+  })
+    .catch((error) => res.json({ msg: error, status: 500 }))
 })
 
 router.get('/api/user/watchlist:id', async (req, res) => {
-    const id = req.params.id;
-    UserModel.findOne({ _id: id })
-        .then(async user => {
-            const watchlist = user.watchlist
-            const movies = []
-            for (let i = 0; i < watchlist.length; i++) {
-                const movie = await MovieModel.findById(watchlist[i])
-                movies.push(movie)
-            }
-            res.json({ movies, status: 200 })
-        })
-        .catch(() => {
-            res.json({ msg: "Erreur innatendue côté serveur", status: 500 })
-        })
+  const id = req.params.id;
+  UserModel.findOne({ _id: id })
+    .then(async user => {
+      const watchlist = user.watchlist
+      const movies = []
+      for (let i = 0; i < watchlist.length; i++) {
+        const movie = await MovieModel.findById(watchlist[i])
+        movies.push(movie)
+      }
+      res.json({ movies, status: 200 })
+    })
+    .catch(() => {
+      res.json({ msg: "Erreur innatendue côté serveur", status: 500 })
+    })
 })
 
 router.post('/api/user/watchlist', (req, res) => {
-    const { _id, email } = req.body;
-    UserModel.updateOne({ email: email }, {
-        $push: {
-            watchlist: { _id: _id }
-        }
-    }).then(() => {
-        res.json({ msg: "Movie added to watchlist", status: 200 })
-    })
-        .catch((error) => res.json({ msg: error, status: 500 }))
+  const { _id, email } = req.body;
+  UserModel.updateOne({ email: email }, {
+    $push: {
+      watchlist: { _id: _id }
+    }
+  }).then(() => {
+    res.json({ msg: "Movie added to watchlist", status: 200 })
+  })
+    .catch((error) => res.json({ msg: error, status: 500 }))
 })
 
 router.delete('/api/user/watchlist:email/:_id', (req, res) => {
-    UserModel.updateOne({ email: req.params.email }, {
-        $pull: {
-            watchlist: { _id: req.params._id }
-        }
+  UserModel.updateOne({ email: req.params.email }, {
+    $pull: {
+      watchlist: { _id: req.params._id }
+    }
+  })
+    .then(() => {
+      res.json({ status: 200 })
     })
-        .then(() => {
-            res.json({ status: 200 })
-        })
-        .catch(() => {
-            console.log("error")
-            res.json({ msg: "Erreur innatendue côté serveur", status: 500 })
-        })
+    .catch(() => {
+      console.log("error")
+      res.json({ msg: "Erreur innatendue côté serveur", status: 500 })
+    })
 })
 
 router.post('/api/user/updateUser', async (req, res) => {
-    try {
-      const {email, username} = req.body;
-      let errors = {};
-  
-      if ((await UserModel.find({email: email})).length) {
-        const filtre = {email: email};
-        const nouvellesValeurs = {username: username};
-        UserModel.updateOne(filtre, nouvellesValeurs).then(resultat => {
-          console.log('Username mise à jour');
-          res.json('username update');
-        })
-      }
-    } catch (err) {
-      err => res.json(err);
-    }
-  });
+  try {
+    const { email, username } = req.body;
+    let errors = {};
 
-  router.post('/api/user/updatePassword', async (req, res) => {
-    try {
-      const {email, password} = req.body;
-      let errors = {};
-  
-      if ((await UserModel.find({email: email})).length) {
-        const filtre = {email: email};
-        const nouvellesValeurs = {password: password};
-        UserModel.updateOne(filtre, nouvellesValeurs).then(resultat => {
-          console.log('Mot de passe mise à jour');
-          res.json('password update');
-        })
-      }
-    } catch (err) {
-      err => res.json(err);
+    if ((await UserModel.find({ email: email })).length) {
+      const filtre = { email: email };
+      const nouvellesValeurs = { username: username };
+      UserModel.updateOne(filtre, nouvellesValeurs).then(resultat => {
+        console.log('Username mise à jour');
+        res.json('username update');
+      })
     }
-  });
+  } catch (err) {
+    err => res.json(err);
+  }
+});
+
+router.post('/api/user/updatePassword', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let errors = {};
+
+    if ((await UserModel.find({ email: email })).length) {
+      const filtre = { email: email };
+      const nouvellesValeurs = { password: password };
+      UserModel.updateOne(filtre, nouvellesValeurs).then(resultat => {
+        console.log('Mot de passe mise à jour');
+        res.json('password update');
+      })
+    }
+  } catch (err) {
+    err => res.json(err);
+  }
+});
 
 
 module.exports = router;
